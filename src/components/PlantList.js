74510e4c49d5a16b6418e6e6308044f91e6ea5cb
samanthakazla <samanthakazla@gmail.com>
2021-05-16 23:31:15 -0500

    mvp

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      plants: [],
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+         .then(res => {
+           this.setState({plants: res.data.plantsData})
+           console.log('Yay! Get request worked!', res)
+         })
+         .catch(err => {
+           console.log('Uh oh! Caught in Axios get request', err);
+         })
+  }
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
